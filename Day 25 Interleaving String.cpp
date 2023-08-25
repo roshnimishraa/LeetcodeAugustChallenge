74c27f1e5cab8d207f87cff7510bb2346d7dac19
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-25 20:18:31 +0530

    Update Day 25 Interleaving String.cpp

--- Day 25 Interleaving String.cpp
+++ Day 25 Interleaving String.cpp
@@ -1,5 +1,6 @@
-Approach Recursion + Memoization 
+Better Approach Recursion + Memoization 
 
+ TC : O(m * n *N ) 
 class Solution {
 public:
  int m,n,N;
@@ -37,3 +38,52 @@ public:
 return solve(0,0,0,s1,s2,s3);
     }
 };
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+Efficient Appraoch 
+ DP 
+ 2 variable i,j
+ k = i+j
+
+ TC : O(m * n) 
+
+ class Solution {
+public:
+ int m,n,N;
+ int t[101][101];
+ bool solve(int i,int j,string &s1,string &s2,string &s3)
+ {
+     if(i==m && j==n && i+j==N){
+         return true;
+     }
+     if(i+j >= N){
+         return false;
+     }
+     if(t[i][j] != -1){
+         return t[i][j];
+     }
+     bool result = false;
+
+     if(s1[i] == s3[i+j]){
+        result= solve(i+1,j,s1,s2,s3);
+     }
+   if(result == true) {
+       return t[i][j]= result;
+   }
+     if(s2[j] == s3[i+j]){
+        result =solve(i,j+1,s1,s2,s3);
+     }
+     return t[i][j] = result;
+ }
+    bool isInterleave(string s1, string s2, string s3) {
+        m = s1.length();
+        n = s2.length();
+        N = s3.length();
+        // initialize
+ memset(t,-1,sizeof(t));
+return solve(0,0,s1,s2,s3);
+    }
+};
+
+
+
+ 
